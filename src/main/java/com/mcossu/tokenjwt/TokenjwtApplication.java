ee0a0d5ff0b842898f68a9e9535eb239d9972992
mcossu <mcossu@intesigroup.com>
2025-07-04 09:31:56 +0200

    Add initial project setup with Maven and Spring Boot

---
+++ src/main/java/com/mcossu/tokenjwt/TokenjwtApplication.java
@@ -0,0 +1,13 @@
+package com.mcossu.tokenjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenjwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcossu/tokenjwt/TokenjwtApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mcossu.tokenjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO document why this method is empty
+    }
+
+}
